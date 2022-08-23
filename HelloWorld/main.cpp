2dd7c8b51c5cbe255dc4fd1a3439ac0c9a88dcc0
Michael Miles <michael@miles-me.net>
2022-08-23 19:39:06 -0400

    Update main.cpp

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -3,16 +3,6 @@
 using namespace std;
 
 int main() {
-	double sales = 95000;
-	const double stateTaxRate = .04;
-	const double countyTaxRate = .02;
-	double stateTax = sales * stateTaxRate;
-	double countyTax = sales * countyTaxRate;
-
-	cout << "Sales: = $" << sales << endl
-		 << "State Tax = $" << stateTax << endl
-		 << "County Tax = $" << countyTax << endl
-		 << "Total Tax = $" << stateTax + countyTax;
 
 	return 0;
 }
