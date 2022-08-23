372627f196a7db5acff73044d9abceec2437583e
Michael Miles <michael@miles-me.net>
2022-08-23 18:37:58 -0400

    The Basics 6-Order of Operations

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 int main() {
-	int x = 10;
-	int y = ++x;
-	std::cout << y;
+	// ()
+	// * and /
+	// + and -
+
+	double x = 10;
+	int y = 5;
+
+	double z = (x + 10) / (3 * y); // Correct solution!
+	std::cout << z;
 	return 0;
 }
