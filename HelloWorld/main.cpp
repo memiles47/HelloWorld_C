e4786943aa8debf5cf502e63e26709b02086f57d
Michael Miles <michael@miles-me.net>
2022-08-24 18:51:03 -0400

    Fundamental Data Types 1- Introduction

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,14 +4,6 @@
 using namespace std;
 
 int main() {
-	// This will be a comment - This comment placement is best
-
-	/*
-	 * This is a multiline comment
-	 */
-
-	int x = 2; // This is another comment - Not the best placement of a comment
-	cout << "I need to have a line to use the variable: "<< x << " squared = " << pow(x, 2);
 
 	return 0;
 }
