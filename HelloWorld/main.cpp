88325f38f685b79b78c512dd050d25faaa31e3ea
Michael Miles <michael@miles-me.net>
2022-08-24 18:28:32 -0400

    The Basics 8-Reading from the Console

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+	double fahrenheit;
+
+	cout << "Please enter a temperature in degrees fahrenheit: ";
+	cin >> fahrenheit;
+
+	double celsius = (fahrenheit - 32) / 1.8000;
+	cout << "That is equivalent to " << celsius << " degrees celsius.";
 
 	return 0;
 }
