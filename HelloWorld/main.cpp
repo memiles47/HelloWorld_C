0aa0bd5525a680c5dfe0150ea88b71336083a84b
Michael Miles <michael@miles-me.net>
2022-08-24 20:27:38 -0400

    Fundamental Data Types 2- Fundamental Data Types

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include <cmath>
+// #include <cmath>
 
 using namespace std;
 
 int main() {
+	// Integer Types
+	short shortType = 0; // Prevalent
+	int intType = 0; // Prevalent
+	long longType = 0;
+	long long longLongType = 0;
+
+	// Floating Point Types
+	float floatType = 0;
+	double doubleType = 0; // Prevalent
+	long double longDoubleType = 0;
+
+	bool boolType = false;
+	char charType = 'A';
+
+	cout << shortType << endl << intType << endl << longType << endl << longLongType << endl;
+	cout << floatType << endl << doubleType << endl << longDoubleType << endl;
+	cout << boolType << endl << charType;
 
 	return 0;
 }
