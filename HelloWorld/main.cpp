6c885126409b27de426d81ff092a17dcb076d0d6
Michael Miles <michael@miles-me.net>
2022-08-25 19:19:53 -0400

    Fundamental Data Types 4- Working with Numbers

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,21 +4,10 @@
 using namespace std;
 
 int main() {
-	auto price = 99.99; // The auto keyword is great for our coding but
-	auto interestRate = 3.67f; // make sure to use the suffix f or L
-	auto fileSize = 9000L;
-	auto letter = 'a';
-	auto isValid = false;
+	auto number = 255;
+	auto numberBinary = 0b11111111;
+	auto numberHexadecimal = 0xFF;
 
-	auto price1{ 99.99 }; // Another way of initializing. Not sure if I like it.
-	auto interestRate1{ 3.67f };
-	auto fileSize1{ 9000L };
-	auto letter1{ 'a' };
-	auto isValid1{ true };
-
-
-	cout << price << endl << interestRate << endl << fileSize << endl << letter << endl << isValid;
-	cout << price1 << endl << interestRate1 << endl << fileSize1 << endl << letter1 << endl << isValid1;
-
+	cout << number << endl << numberBinary << endl << numberHexadecimal;
 	return 0;
 }
