8bb86fd160af3a4ee3f6976f26b92c7e7cd8c497
Michael Miles <michael@miles-me.net>
2022-08-25 19:25:31 -0400

    Fundamental Data Types 4- Working with Numbers

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,6 +8,9 @@ int main() {
 	auto numberBinary = 0b11111111;
 	auto numberHexadecimal = 0xFF;
 
-	cout << number << endl << numberBinary << endl << numberHexadecimal;
+	// Can not used auto with unsigned. This can not accept negative numbers. Stay away
+	unsigned int unsignedNumber = 255; 
+
+	cout << number << endl << numberBinary << endl << numberHexadecimal << endl << endl << unsignedNumber;
 	return 0;
 }
