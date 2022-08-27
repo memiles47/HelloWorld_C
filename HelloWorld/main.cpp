127ce867880b97c92fdbcd1ea5bc729ce9d7e9a3
Michael Miles <michael@miles-me.net>
2022-08-27 08:51:20 -0400

    Fundamental Data Types 5- Narrowing

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,13 +4,6 @@
 using namespace std;
 
 int main() {
-	auto number = 255;
-	auto numberBinary = 0b11111111;
-	auto numberHexadecimal = 0xFF;
 
-	// Can not used auto with unsigned. This can not accept negative numbers. Stay away
-	unsigned int unsignedNumber = 255; 
-
-	cout << number << endl << numberBinary << endl << numberHexadecimal << endl << endl << unsignedNumber;
 	return 0;
 }
