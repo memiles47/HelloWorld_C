4896688f8757ac7fc4f04852751471db4096627f
Michael Miles <michael@miles-me.net>
2022-08-28 14:00:16 -0400

    Fundamental Data Types 6- Generating Random Numbers

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <cstdlib>;
+#include <ctime>;
 // #include <cmath>
 
 using namespace std;
 
 int main() {
-	int number{ 1'000'000 };
-	short another = number;
-	// short another2{ number }; // Will not compile
-	cout << another;
+	const auto minValue{ 1 };
+	const auto maxValue{ 6 };
+
+	srand(time(nullptr));
+	auto number{( rand() % (maxValue - minValue + 1)) + minValue };
+	cout << number;
 	return 0;
 }
