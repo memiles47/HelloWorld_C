cffad508f74f1bb91ad746532c6eb33cdf906549
Michael Miles <michael@miles-me.net>
2022-08-28 15:15:38 -0400

    Fundamental Data Types 8- Data Types Size and Limits

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 // #include <cstdlib>;
 // #include <ctime>;
-#include <iomanip>;
+// #include <iomanip>;
 // #include <cmath>
 
 using namespace std;
 
 int main() {
-	cout << left;
-	cout << setw(15) << "Course" << setw(10) << "Students" << endl;
+	cout << sizeof(int) << endl
+		 << numeric_limits<int>::max() << endl
+		 << numeric_limits<int>::min() << endl;
 
-	cout << setw(15) << "C++"        << right << setw(10) << "100" << endl;
-	cout << left
-		 << setw(15) << "JavaScript" << right << setw(10) << "50";
-
+	cout << sizeof(double) << endl
+		 << numeric_limits<double>::max() << endl
+		 << numeric_limits<double>::min();
 
 	return 0;
 }
