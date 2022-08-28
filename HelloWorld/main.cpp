4e17476e9824bf28e706864e29ddf15d73d66ad1
Michael Miles <michael@miles-me.net>
2022-08-28 18:43:48 -0400

    Fundamental Data Types 10- Working with Characters and Strings

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 // #include <cstdlib>;
 // #include <ctime>;
 // #include <iomanip>;
@@ -7,7 +8,30 @@
 using namespace std;
 
 int main() {
-	auto isNewUser = false;
-	cout << boolalpha << isNewUser;
+	string name{};
+	cout << "Enter your name: ";
+	getline(cin, name);
+
+	string street{};
+	cout << endl << "Enter your address" << endl
+		 << "Street: ";
+	getline(cin, street);
+
+	string city{};
+	cout << "City: ";
+	getline(cin, city);
+
+	string state{};
+	cout << "State: ";
+	getline(cin, state);
+
+	string zip{};
+	cout << "Zip: ";
+	cin  >> zip;
+
+	cout << name << endl
+		 << street << endl
+		 << city << ", " << state << ", " << zip;
+
 	return 0;
 }
