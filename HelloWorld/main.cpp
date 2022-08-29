36c9293af82fe627b4ac75b9342f72a0dbd24ec4
Michael Miles <michael@miles-me.net>
2022-08-29 17:37:22 -0400

    Fundamental Data Types 11- Working with Arrays2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-// #include <string>
+#include <string>
 // #include <cstdlib>;
 // #include <ctime>;
-// #include <iomanip>;
+//#include <iomanip>;
 // #include <cmath>
 
 using namespace std;
@@ -10,13 +10,14 @@ using namespace std;
 int main() {
 	string names[3];
 	cout << "Enter Name 1: ";
-	cin >> names[0];
+	getline(cin, names[0]);
 
-	cout << "Enter Name 2: ";
-	cin >> names[1];
+	cout << "Enter Name 1: ";
+	getline(cin, names[1]);
 
-	cout << "Enter Name 3: ";
-	cin >> names[2];
+	cout << "Enter Name 1: ";
+	getline(cin, names[2]);
+
 
 	cout << "The first name you entered was: " << names[0];
 	return 0;
