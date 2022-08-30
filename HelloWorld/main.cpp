c20db8c1987297e4b04523074b6f45e68383097a
Michael Miles <michael@miles-me.net>
2022-08-30 18:03:59 -0400

    Decision Making 4- Order of Logical Operators

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,15 +8,26 @@
 using namespace std;
 
 int main() {
+	// Order of logical operators
 	// ()
 	// !
 	// &&
 	// ||
 
-	bool a{ true };
-	bool b{ false };
-	bool c{ false };
-	bool result = a || b && c;
-	cout << boolalpha << result << endl << c;
+	char isUSCitizen{};
+	cout << "Are you a US citizen (Y/N)?: ";
+	cin >> isUSCitizen;
+
+	char hasBachelorsDegree{};
+	cout << "Do you have a Bachelors degree (Y/N)?: ";
+	cin >> hasBachelorsDegree;
+
+	int yearsOfExperience{};
+	cout << "How many years of experience do you have?: ";
+	cin >> yearsOfExperience;
+
+	bool isEligible = isUSCitizen == 'Y' && (hasBachelorsDegree == 'Y' || yearsOfExperience >= 2);
+	cout << boolalpha << isEligible;
+
 	return 0;
 }
