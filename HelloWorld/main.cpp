0d56a25f9f21e723cffc3de4d5c710be6e77d1a4
Michael Miles <michael@miles-me.net>
2022-08-31 17:00:42 -0400

    Decision Making 6- Nested If Statements

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,16 +8,6 @@
 using namespace std;
 
 int main() {
-	double sales{};
-	double commission;
 
-	cout << "Enter sales total: ";
-	cin >> sales;
-
-	if (sales <= 10'000) commission = .10;
-	else if (sales <= 15'000) commission = .15;
-	else commission = .20;
-
-	cout << endl << "Your commission will be " << commission * 100 << "%";
 	return 0;
 }
