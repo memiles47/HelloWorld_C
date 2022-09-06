fdd0ce5ff0a787336547167b58eaccb1929275d8
Michael Miles <michael@miles-me.net>
2022-09-06 18:53:28 -0400

    Decision Making 8- Exercise

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,35 +9,6 @@ using namespace std;
 
 int main()
 {
-	cout << "1- Create account" << endl
-		 << "2- Change password" << endl
-		 << "3- Quit" << endl
-		 << "Select an option: ";
-
-	short input;
-	cin >> input;
-
-	switch (input)
-	{
-	case 1:
-		cout << "You selected 1";
-		break;
-
-	case 2:
-		cout << "You selectedd 2";
-		break;
-
-	default:
-		cout << "Goodbye!";
-	}
-
-
-	if (input == 1)
-		cout << "You selected 1";
-	else if (input == 2)
-		cout << "You selected 2";
-	else
-		cout << "Goodbye!";
 
 
 	return 0;
