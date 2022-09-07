92cf3ad189d7e1225de117f0e4e1244fbc3127fe
Michael Miles <michael@miles-me.net>
2022-09-07 17:51:55 -0400

    Loops 2- The for Loop: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,36 +9,7 @@ using namespace std;
 
 int main()
 {
-	double value1{};
-	double value2{};
-	double answer{};
-	char op{};
 
-	cout << "Enter two values and an operator seperated by spaces" << endl;
-	cin >> value1 >> value2 >> op;
 
-	switch (op)
-	{
-	case '*':
-		answer = value1 * value2;
-		break;
-
-	case '/':
-		answer = value1 / value2;
-		break;
-
-	case '+':answer = value1 + value2;
-		break;
-
-	case '-':
-		answer = value1 - value2;
-		break;
-
-	default:
-		cout << "Invalid operator!";
-	}
-
-	cout << value1 << " " << op << " " << value2 << " = " << answer;
-	
 	return 0;
 }
