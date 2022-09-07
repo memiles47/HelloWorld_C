fe76f7b0a758cb4e0a876721643a17cee9930729
Michael Miles <michael@miles-me.net>
2022-09-06 20:13:46 -0400

    Decision Making 8- Exercise Solution

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,7 +9,36 @@ using namespace std;
 
 int main()
 {
+	double value1{};
+	double value2{};
+	double answer{};
+	char operation{};
 
+	cout << "Enter two values and an operator seperated by spaces" << endl;
+	cin >> value1 >> value2 >> operation;
 
+	switch (operation)
+	{
+	case '*':
+		answer = value1 * value2;
+		break;
+
+	case '/':
+		answer = value1 / value2;
+		break;
+
+	case '+':answer = value1 + value2;
+		break;
+
+	case '-':
+		answer = value1 - value2;
+		break;
+
+	default:
+		cout << "Invalid operator";
+	}
+
+	cout << value1 << " " << operation << " " << value2 << " = " << answer;
+	
 	return 0;
 }
