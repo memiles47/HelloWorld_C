75c1266badd17df1d618484cfe87b33a9cb85bac
Michael Miles <michael@miles-me.net>
2022-09-07 18:01:12 -0400

    Loops 2- The for Loop: 4

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,7 +9,10 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 5; i++)
-		cout << i << endl;
+	for(int i = 5; i > 0; i--)
+	{
+		if (i % 2 != 0)
+			cout << i << endl;
+	}
 	return 0;
 }
