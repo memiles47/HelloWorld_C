48f97908d09002e06595c1280ea2f3ec9d4893dc
Michael Miles <michael@miles-me.net>
2022-09-11 15:08:25 -0400

    Loops 3- Range-based for Loops: 2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,6 +9,17 @@ using namespace std;
 
 int main()
 {
+	int numbers[] = { 1, 2, 3, 4 };
+	string name{ "Michael Miles" };
+
+	for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
+		cout << numbers[i] << endl;
+
+	for (int number: numbers)
+		cout << number << endl;
+
+	for (char ch : name)
+		cout << ch;
 
 	return 0;
 }
