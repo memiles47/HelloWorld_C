b3b7a48b6b315d570ef6b821780e914f2f7fe20e
Michael Miles <michael@miles-me.net>
2022-09-11 17:50:44 -0400

    Loops 4- While Loops: 2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,6 +9,24 @@ using namespace std;
 
 int main()
 {
+	for (int i = 1; i <= 5; i++)
+		cout << i << endl;
 
+	int i{ 1 };
+	while (i <= 5)
+	{
+		cout << i << endl;
+		i++;
+	}
+	
+	bool validNumber{ true };
+	int number{};
+	while(validNumber)
+	{
+		cout << "Enter a number: ";
+		cin >> number;
+
+		validNumber = !((number >= 0) && (number <= 5));
+	}
 	return 0;
 }
