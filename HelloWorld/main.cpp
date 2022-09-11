112b8f6ee4e2500b74af076fd779d84b6078f862
Michael Miles <michael@miles-me.net>
2022-09-11 09:23:25 -0400

    Loops 3- Range-based for Loops: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,21 +9,6 @@ using namespace std;
 
 int main()
 {
-	cout << "Please enter a number to factor, 0 or above: ";
-
-	long number{1};
-	cin >> number;
-
-	long factorial{1};
-	for(long i = 2; i <= number; i++)
-	{
-		if (number == 0 || number == 1)
-			break;
-
-		factorial *= i;
-	}
-
-	cout << "The factorial of is " << factorial;
 
 	return 0;
 }
