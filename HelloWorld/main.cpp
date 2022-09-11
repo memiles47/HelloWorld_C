8ed3e9a3322c052446bc3ad58afb388599eb6929
Michael Miles <michael@miles-me.net>
2022-09-11 16:51:47 -0400

    Loops 4- While Loops: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,17 +9,6 @@ using namespace std;
 
 int main()
 {
-	int temperatures[]{ 80, 78, 95, 75 };
-	double sum{};
-
-	for (int temperature : temperatures)
-		sum += temperature;
-
-	short count{ sizeof(temperatures) / sizeof(int) };
-	
-	double average = sum / count;
-	cout << average;
-
 
 	return 0;
 }
