0f0b5f038e2af77072fb1708c30a8ddae7b80d71
Michael Miles <michael@miles-me.net>
2022-09-12 18:58:24 -0400

    Functions 2- Defining and Calling Functions: 2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,8 +7,17 @@
 
 using namespace std;
 
+void greet()
+{
+	cout << "Hello World" << endl;
+}
+
 int main()
 {
-	
+	// Calling - Invoking - executing
+	greet();
+
+	cout << "Done";
+
 	return 0;
 }
