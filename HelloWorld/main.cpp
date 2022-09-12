2361815b566c4be3d6f327ec22235399dce787fe
Michael Miles <michael@miles-me.net>
2022-09-12 18:49:13 -0400

    Functions 2- Defining and Calling Functions: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -9,18 +9,6 @@ using namespace std;
 
 int main()
 {
-	cout << "Please enter number of rows: ";
-	int number{};
-	cin >> number;
-	char ch = '*';
-
-	for (int i = 1; i <= number; i++)
-	{
-		for (int a = 1; a <= i; a++)
-			cout << ch;
-		cout << endl;
-	}
-
-
+	
 	return 0;
 }
