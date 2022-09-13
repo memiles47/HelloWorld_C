9b827baf3e62a21e560f3ef28a04b36f71470f9b
Michael Miles <michael@miles-me.net>
2022-09-13 19:10:11 -0400

    Functions 3- Parameters with a Default Value: 2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,8 +7,17 @@
 
 using namespace std;
 
+
+double calculateTax(double income, double taxRate = .2)
+{
+	return income * taxRate;
+}
+
 int main()
 {
-
+	double taxNoDefault = calculateTax(10'000, .3);
+	double tax = calculateTax(10'000);
+	cout << tax << endl;
+	cout << taxNoDefault << endl;
 	return 0;
 }
