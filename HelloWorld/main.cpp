bfee8c1d75cea60b497a393bbbbebf692ecc625a
Michael Miles <michael@miles-me.net>
2022-09-13 19:12:50 -0400

    Functions 4- Overloading Functions: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,17 +7,7 @@
 
 using namespace std;
 
-// Always code the parameters with a default at the end of the parameter list
-double calculateTax(double income, double taxRate = .2)
-{
-	return income * taxRate;
-}
-
 int main()
 {
-	double taxNoDefault = calculateTax(10'000, .3);
-	double tax = calculateTax(10'000);
-	cout << tax << endl;
-	cout << taxNoDefault << endl;
 	return 0;
 }
