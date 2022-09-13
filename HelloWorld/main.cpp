540fc09b2b978262aaf3763954f1a5d3693759e6
Michael Miles <michael@miles-me.net>
2022-09-12 20:12:26 -0400

    Functions 2- Parameters with a Default Value: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,19 +7,8 @@
 
 using namespace std;
 
-int largest(int first, int last)
-{
-	return first > last ? first : last;
-}
-
 int main()
 {
-	cout << "Please Enter two integers separated by a space followed by the <ENTER> key: ";
-	int x{};
-	int y{};
-	cin >> x >> y;
-
-	cout << largest(x, y);
 
 	return 0;
 }
