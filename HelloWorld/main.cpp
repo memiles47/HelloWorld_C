918a53e6c48c453933196560e5bdb7c3aadcfc1d
Michael Miles <michael@miles-me.net>
2022-09-13 19:24:16 -0400

    Functions 5- Passing Arguments by Value or Reference: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,20 +7,7 @@
 
 using namespace std;
 
-// Signature of a function = name + (number and type of parameters)
-// When overloading functions each function signature must be unique
-void greet(string name)
-{
-	cout << "Hello " << name << endl;
-}
-
-void greet(string title, string name)
-{
-	cout << "Hello " << title << " " << name << endl;
-}
-
 int main()
 {
-	greet("Mr.", "Miles");
 	return 0;
 }
