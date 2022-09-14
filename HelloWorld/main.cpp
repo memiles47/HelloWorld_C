13c45451cd20757d29685a3b0f33418cf3876267
Michael Miles <michael@miles-me.net>
2022-09-14 18:15:35 -0400

    Function 6- Local vs Global Variables: 1

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,32 +7,7 @@
 
 using namespace std;
 
-/*
-Adding an & after the type creates a reference to the original variable
-this should be done with variable containing large amounts of date e.g. strings
-*/
-
-void increasePrice(double& price)
-{
-	price *= 1.2;
-}
-
-void greet(const string& name)
-{
-	cout << "Hello " << name << endl;
-	// name = "a";
-}
-
 int main()
 {
-	double price{ 100 };
-	increasePrice(price);
-	cout << price << endl;
-
-	string name{ "Michael" };
-	greet(name);
-	cout << name;
-	
-
 	return 0;
 }
